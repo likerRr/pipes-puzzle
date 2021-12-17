b62c71826ccd417a9a2dc9ca0783be0dc6842a6d
Alexey Lizurchik <al.lizurchik@gmail.com>
2021-12-17 16:42:38 +0100

    Add base game config

---
+++ src/game/config.ts
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 800,
+  backgroundColor: '#6DD0F7',
+  disableContextMenu: true,
+}
+
+export default config;

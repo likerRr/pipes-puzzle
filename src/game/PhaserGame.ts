e99d2483eba6183d8106c9ae60d49b57680a760d
Alexey Lizurchik <al.lizurchik@gmail.com>
2021-12-17 16:42:19 +0100

    Add PhaserGame class for initializing a game

---
+++ src/game/PhaserGame.ts
@@ -0,0 +1,17 @@
+import Phaser from 'phaser';
+
+class PhaserGame {
+  constructor(
+    private config: Phaser.Types.Core.GameConfig
+  ) {}
+
+  start(container: HTMLElement, scenes: Array<Phaser.Scene> = []): Phaser.Game {
+    return new Phaser.Game({
+      ...this.config,
+      parent: container,
+      scene: scenes,
+    });
+  }
+}
+
+export default PhaserGame;

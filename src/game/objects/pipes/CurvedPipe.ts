b960192f25073cbdc610aed1abf3a57fee2d7855
Alexey Lizurchik <al.lizurchik@gmail.com>
2021-12-18 23:24:53 +0100

    Add pipes variants

---
+++ src/game/objects/pipes/CurvedPipe.ts
@@ -0,0 +1,12 @@
+import Phaser from 'phaser';
+
+import { TEXTURE_PIPES } from '../../../constants/textureName';
+import PipeTile from './PipeTile';
+
+class CurvedPipe extends PipeTile {
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    super(scene, x, y, TEXTURE_PIPES, '┏', ['┏', '┓', '┛', '┗']);
+  }
+}
+
+export default CurvedPipe;

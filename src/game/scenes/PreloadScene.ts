5a328411b6ccb59129c0ef802e832094a80d2f2b
Alexey Lizurchik <al.lizurchik@gmail.com>
2021-12-24 15:19:43 +0100

    Load and start game from preload screen

--- src/game/scenes/PreloadScene.ts
+++ src/game/scenes/PreloadScene.ts
@@ -1,4 +1,8 @@
 import { SCENE_MENU, SCENE_PRELOAD } from '../../constants/sceneName';
+import BrowserWebSocketService from '../../lib/ws/BrowserWebSocketService';
+import WebSocketClient from '../../lib/wsClient/WebSocketClient';
+import Game from '../features/gameScene/Game';
+import WsClientRegistry from '../features/registry/WsClientRegistry';
 import PipesTexture from '../resources/PipesTexture';
 
 class PreloadScene extends Phaser.Scene {
@@ -11,7 +15,42 @@ class PreloadScene extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start(SCENE_MENU);
+    let reconnecting = false;
+    let reconnectingAttempts = 0;
+    const reconnectingAttemptsLimit = 5;
+
+    const ws = new BrowserWebSocketService({
+      address: process.env.NEXT_PUBLIC_WEBSOCKET_URL as string,
+      onClose: () => {
+        if (reconnectingAttempts >= reconnectingAttemptsLimit) {
+          // TODO show something
+          return;
+        }
+
+        this.time.delayedCall(2000 * Math.max(1, reconnectingAttempts), () => {
+          ws.connect();
+
+          reconnectingAttempts += 1;
+          reconnecting = true;
+        });
+      },
+      onOpen: () => {
+        if (reconnecting) {
+          Game.restartScenesToMenu(this);
+        }
+
+        reconnectingAttempts = 0;
+        reconnecting = false;
+
+        const wsClient = new WebSocketClient(ws);
+
+        WsClientRegistry.setWsClient(this.registry, wsClient);
+
+        this.scene.start(SCENE_MENU);
+      },
+    });
+
+    ws.connect();
   }
 }
 

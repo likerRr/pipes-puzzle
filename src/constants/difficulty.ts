ac8e67f20af4138ef0d9634143cbc8bfb486a071
Alexey Lizurchik <al.lizurchik@gmail.com>
2021-12-24 15:22:09 +0100

    Add difficulties map

---
+++ src/constants/difficulty.ts
@@ -0,0 +1,12 @@
+type Difficulty = { [n: number]: string };
+
+const difficulty: Difficulty = {
+  1: 'Easy',
+  2: 'Normal',
+  3: 'Hard',
+  4: 'Nightmare',
+  5: 'Hell',
+  6: 'Genius',
+};
+
+export default difficulty;
